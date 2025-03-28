639ba1e021b8ce5a569db724840a77793a2687ec
Marzano <arthuramarzano10@gmail.com>
2025-03-28 13:28:31 -0300

    começando a criar a organização do projeto...

---
+++ TCC/BackEnd/TCC/src/main/java/TCC/TCC/Repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package TCC.TCC.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import TCC.TCC.Repository.*;
+import TCC.TCC.model.*;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
