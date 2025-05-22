abfff207111f176237c18a4d5e543cbf341e5d93
Marzano <arthuramarzano10@gmail.com>
2025-05-22 15:17:15 -0300

    colocando o subnav na página de deixando lá em baixo

--- TCC/frontend/src/Pages/Gallery/index.jsx
+++ TCC/frontend/src/Pages/Gallery/index.jsx
@@ -1,11 +1,24 @@
-import NavBar from "../../Components/NavBar"
+import styled from "styled-components";
+import NavBar from "../../Components/Navs/NavBar"
+import Subnav from "../../Components/Navs/Subnav";
 
 const Gallery = () => {
     return (
         <>
             <NavBar/>
+            <SubnavWrapper>
+                <Subnav/>
+            </SubnavWrapper>
         </>
     )
 }
 
 export default Gallery;
+
+
+const SubnavWrapper = styled.div`
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+`
