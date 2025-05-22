99b5b1dcaa11084991101124f57ff8e8ebc6651b
Marzano <arthuramarzano10@gmail.com>
2025-05-22 14:34:40 -0300

    criando card perfil

---
+++ TCC/frontend/src/Components/Cards/CardPerfil/index.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import LogoutBTN from "../../Buttons/LogoutBTN"
+import profilePictureWhite from "../../../Assets/SVGs/Icons/icon-profile-white&purple.svg";
+
+const CardPerfil = () => {
+    
+    return (
+        <Wrapper>
+            <ProfileWrapper>
+                <ProfilePciture src={profilePictureWhite}/>
+                <ProfileName>Convidado</ProfileName>
+            </ProfileWrapper>
+            <LogoutBTN/>
+        </Wrapper>
+    )
+}
+
+export default CardPerfil;
+
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+    align-items: center;
+    width: 10%;
+    height: 73%;
+    background-color: #000000;
+    border-radius: 0.79rem;
+    margin: 7px;
+    padding: 7px;
+`
+
+const ProfileWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: left;
+`
+
+const ProfilePciture = styled.img`
+    height: 80%;
+    cursor: pointer;
+`
+const ProfileName = styled.p`
+    color: #FFF;
+    margin: 10px;
+    cursor: pointer;
+`
