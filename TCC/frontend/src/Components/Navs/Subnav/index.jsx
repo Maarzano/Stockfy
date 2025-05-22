f444d1ae7c08145d3358e6ef23cf51ef07cd71b0
Marzano <arthuramarzano10@gmail.com>
2025-05-22 15:27:32 -0300

    criando componente para um pop up com nome

--- TCC/frontend/src/Components/Navs/Subnav/index.jsx
+++ TCC/frontend/src/Components/Navs/Subnav/index.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 import SettingIcon from "../../../Assets/SVGs/Icons/icon-setting-purple.svg";
+import ToolTipTab from '../../ToolTipTab';
 
 const Subnav = () => {
   return (
     <StyledWrapper>
       <div className="navigation-card">
-        <a href="#" className="tab">
-          <svg className="svgIcon" viewBox="0 0 104 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100.5 40.75V96.5H66V68.5V65H62.5H43H39.5V68.5V96.5H3.5V40.75L52 4.375L100.5 40.75Z" stroke="#623bda" strokeWidth={7} />
-          </svg>
-        </a>
-        <a href="#" className="tab">
-          <svg width={104} height={100} viewBox="0 0 104 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="21.5" y="3.5" width={60} height={60} rx={30} stroke="#623bda" strokeWidth={7} />
-            <g clipPath="url(#clip0_41_27)">
-              <mask id="mask0_41_27" style={{maskType: 'luminance'}} maskUnits="userSpaceOnUse" x={0} y={61} width={104} height={52}>
-                <path d="M0 113C0 84.2812 23.4071 61 52.1259 61C80.706 61 104 84.4199 104 113H0Z" fill="#623bda" />
-              </mask>
-              <g mask="url(#mask0_41_27)">
-                <path d="M-7 113C-7 80.4152 19.4152 54 52 54H52.2512C84.6973 54 111 80.3027 111 112.749H97C97 88.0347 76.9653 68 52.2512 68H52C27.1472 68 7 88.1472 7 113H-7ZM-7 113C-7 80.4152 19.4152 54 52 54V68C27.1472 68 7 88.1472 7 113H-7ZM52.2512 54C84.6973 54 111 80.3027 111 112.749V113H97V112.749C97 88.0347 76.9653 68 52.2512 68V54Z" fill="#623bda" />
-              </g>
-            </g>
-            <defs>
-              <clipPath id="clip0_41_27">
-                <rect width={104} height={39} fill="white" transform="translate(0 61)" />
-              </clipPath>
-            </defs>
-          </svg>
-        </a>
-        <a href="#" className="tab">
-          <img src={SettingIcon} alt=""/>
-        </a>
+        <ToolTipTab label={"Galeria"}>
+            <a href="#" className="tab">
+            <svg className="svgIcon" viewBox="0 0 104 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100.5 40.75V96.5H66V68.5V65H62.5H43H39.5V68.5V96.5H3.5V40.75L52 4.375L100.5 40.75Z" stroke="#623bda" strokeWidth={7} />
+            </svg>
+            </a>
+        </ToolTipTab>
+
+        <ToolTipTab label={"Perfil"}>
+            <a href="#" className="tab">
+            <svg width={104} height={100} viewBox="0 0 104 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="21.5" y="3.5" width={60} height={60} rx={30} stroke="#623bda" strokeWidth={7} />
+                <g clipPath="url(#clip0_41_27)">
+                <mask id="mask0_41_27" style={{maskType: 'luminance'}} maskUnits="userSpaceOnUse" x={0} y={61} width={104} height={52}>
+                    <path d="M0 113C0 84.2812 23.4071 61 52.1259 61C80.706 61 104 84.4199 104 113H0Z" fill="#623bda" />
+                </mask>
+                <g mask="url(#mask0_41_27)">
+                    <path d="M-7 113C-7 80.4152 19.4152 54 52 54H52.2512C84.6973 54 111 80.3027 111 112.749H97C97 88.0347 76.9653 68 52.2512 68H52C27.1472 68 7 88.1472 7 113H-7ZM-7 113C-7 80.4152 19.4152 54 52 54V68C27.1472 68 7 88.1472 7 113H-7ZM52.2512 54C84.6973 54 111 80.3027 111 112.749V113H97V112.749C97 88.0347 76.9653 68 52.2512 68V54Z" fill="#623bda" />
+                </g>
+                </g>
+                <defs>
+                <clipPath id="clip0_41_27">
+                    <rect width={104} height={39} fill="white" transform="translate(0 61)" />
+                </clipPath>
+                </defs>
+            </svg>
+            </a>
+        </ToolTipTab>
+
+        <ToolTipTab label={"Configurações"}>
+            <a href="#" className="tab">
+                <img src={SettingIcon} alt=""/>
+            </a>
+        </ToolTipTab>
       </div>
     </StyledWrapper>
   );
---
+++ TCC/frontend/src/Components/ToolTipTab/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ToolTipTab = ({ children, label }) => {
+  return (
+    <TabWrapper>
+      {children}
+      <Tooltip>{label}</Tooltip>
+    </TabWrapper>
+  );
+};
+
+const TabWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover span {
+    opacity: 1;
+    transform: translateY(-10px);
+  }
+`;
+
+const Tooltip = styled.span`
+  position: absolute;
+  bottom: 115%;
+  background-color: #333;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 6px;
+  font-size: 12px;
+  white-space: nowrap;
+  opacity: 0;
+  transform: translateY(0);
+  transition: opacity 0.3s ease, transform 0.3s ease;
+  pointer-events: none;
+  z-index: 10;
+`;
+
+export default ToolTipTab;
