d3327cc7f60a9382ea943e19dc6f49dd170c9b6e
Maarzano <arthuramarzano10@gmail.com>
2025-05-21 21:42:55 -0300

    colocando página nas routes

--- TCC/frontend/src/Routes/index.jsx
+++ TCC/frontend/src/Routes/index.jsx
@@ -2,11 +2,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Auth from "../Pages/Auth";
 import Home from '../Pages/Home'
 import NotFound from "../Pages/NotFound";
+import Gallery from "../Pages/Gallery"
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Auth />} />
+            <Route path="/Gallery" element={<Gallery/>} />
             <Route path="/Home" element={<Home/>}/>
             <Route path="*" element={<NotFound/>} />
         </Routes>
