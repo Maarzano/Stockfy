544864f8f3500d773392288871823f24972874a5
Marzano <arthuramarzano10@gmail.com>
2025-05-22 14:35:43 -0300

    colocando card perfil no Navbar e colocando card carrinho...
    o Navbar ainda contém erros presentes como o botão logout sobrepondo outras coisas, e título muito feio e saindo do Navbar

--- TCC/frontend/src/Components/NavBar/index.jsx
+++ TCC/frontend/src/Components/NavBar/index.jsx
@@ -1,20 +1,50 @@
 import styled from "styled-components"
-import LogoutBTN from "../Buttons/LogoutBTN"
+import CardPerfil from "../Cards/CardPerfil"
+import Cart from "../../Assets/SVGs/Icons/icon-cart-black.svg"
 
 const NavBar = () =>{
     
     
     return (
         <Wrapper>
-            <LogoutBTN/>
+            <WrapperCart>
+                <CartImg src={Cart}/>
+            </WrapperCart>
+            <SystemName>Stockify</SystemName>
+            <CardPerfil/>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    height: 5.5vw;
-    background-color: black;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: 4vw;
+    background-color: #623bda;
     width: 100%;
+`
+
+const WrapperCart = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: left;
+`
+
+const CartImg = styled.img`
+    width: 55px;
+    margin-left: 85px;
+    cursor: pointer;
+`
+
+const SystemName = styled.h1`
+    color: #FFF;
+    font-size: 60px;
+    padding: 10px;
+    height: fit-content;
+    cursor: pointer;
 `
 
 export default NavBar
