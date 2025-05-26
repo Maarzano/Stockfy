d5b1f6c959c12bbad02984c0c7e1828cc0d19999
Marzano <arthuramarzano10@gmail.com>
2025-05-26 16:46:11 -0300

    colocando campo de pesquisa

--- TCC/frontend/src/Components/GalleryItemSection/index.jsx
+++ TCC/frontend/src/Components/GalleryItemSection/index.jsx
@@ -6,14 +6,17 @@ const GalleryItenSection = () => {
     return (
         <Wrapper>
             <p>Bem-vindo à tela de estoque. Nesta seção, é possível visualizar a quantidade atual de cada produto disponível na empresa. Este espaço foi desenvolvido para facilitar o acompanhamento dos itens armazenados e permitir o controle das saídas de produtos de forma prática e organizada. Mantenha-se sempre atualizado com as quantidades em tempo real e garanta uma gestão eficiente dos recursos.</p>
+            <WrapperSearch>
+                <Search />
+            </WrapperSearch>
             <WrapperInside>
                 
                 <CardItem imgURL={"https://www.baip.com.br/image/cache/data/eftr/Img_ftr_rp_320401-580x580.PNG?version=20250514104840"} 
                     tittle="Martelo" description="martelo que usamos para tudo"/>
                 <CardItem/>
                 <CardItem/>
-                <CardItem/>
-                <CardItem/> {/* ainda ovu arrumar isso e acessar via backend */}
+                <CardItem imgURL={"https://lojatexugo.cdn.magazord.com.br/img/2022/07/produto/2757/lampada-led-filamento-blumenau-4w-bivolt-a60-e27-2200k-luz-amarela.jpg?ims=800x800"} tittle={"Lâmpada"} description={"lampada para trocar se nescessário"}/>
+                <CardItem/> {/* ainda vou arrumar isso e acessar via backend */}
                 <CardItem/> {/* TODO */}
             </WrapperInside>
         </Wrapper>
@@ -32,10 +35,17 @@ const Wrapper = styled.div`
 const WrapperInside = styled.div`
     display: flex;
     flex-wrap: wrap;
-    margin: 40px;
+    margin: 20px 40px 40px 40px;
     background-color: #1a1a1a;
     border-radius: 15px;
     padding: 20px;
+`
+
+const WrapperSearch = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+    justify-content: left;
 `
 
 export default GalleryItenSection;
