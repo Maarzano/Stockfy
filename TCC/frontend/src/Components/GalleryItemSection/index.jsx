691d15e33a6410453c9490e7c193e127d0654c28
Marzano <arthuramarzano10@gmail.com>
2025-05-27 11:23:19 -0300

    mapeando o CardItem para que traga os itens do backend

--- TCC/frontend/src/Components/GalleryItemSection/index.jsx
+++ TCC/frontend/src/Components/GalleryItemSection/index.jsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import CardItem from "../Cards/CardItem";
 import Search from "../Search";
+import { useProdutos } from "../../Hooks/useProdutos";
 
 const GalleryItenSection = () => {
+    const { produtos, loading, erro } = useProdutos();
+
+    if (erro) alert(`Erro: ${erro}`);
+
     return (
         <Wrapper>
             <p>Bem-vindo à tela de estoque. Nesta seção, é possível visualizar a quantidade atual de cada produto disponível na empresa. Este espaço foi desenvolvido para facilitar o acompanhamento dos itens armazenados e permitir o controle das saídas de produtos de forma prática e organizada. Mantenha-se sempre atualizado com as quantidades em tempo real e garanta uma gestão eficiente dos recursos.</p>
@@ -10,14 +15,15 @@ const GalleryItenSection = () => {
                 <Search />
             </WrapperSearch>
             <WrapperInside>
+                {produtos.map((produto) => (
+                    <CardItem 
+                    key={produto.itemId}
+                    imgURL={produto.imagem}
+                    tittle={produto.nomeItem}
+                    description={produto.descricao}
+                    />
+                ))}
                 
-                <CardItem imgURL={"https://www.baip.com.br/image/cache/data/eftr/Img_ftr_rp_320401-580x580.PNG?version=20250514104840"} 
-                    tittle="Martelo" description="martelo que usamos para tudo"/>
-                <CardItem/>
-                <CardItem/>
-                <CardItem imgURL={"https://lojatexugo.cdn.magazord.com.br/img/2022/07/produto/2757/lampada-led-filamento-blumenau-4w-bivolt-a60-e27-2200k-luz-amarela.jpg?ims=800x800"} tittle={"Lâmpada"} description={"lampada para trocar se nescessário"}/>
-                <CardItem/> {/* ainda vou arrumar isso e acessar via backend */}
-                <CardItem/> {/* TODO */}
             </WrapperInside>
         </Wrapper>
     )
@@ -30,6 +36,7 @@ const Wrapper = styled.div`
     background-color: black;
     color: wheat;
     padding: 30px;
+    height: 100%;
 `
 
 const WrapperInside = styled.div`
