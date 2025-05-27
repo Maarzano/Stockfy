d341530c7c564e97b06bea562c04cc4f5de81635
Marzano <arthuramarzano10@gmail.com>
2025-05-27 11:22:56 -0300

    criando hook personalizado para trazer produtos carregamento e erro se tiver algum...

---
+++ TCC/frontend/src/Hooks/useProdutos.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { buscarProdutos } from "../Services/prudutoService";
+
+export const useProdutos = () => {
+    const [produtos, setProdutos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [erro, setErro] = useState(null);
+
+
+    useEffect(() => {
+        async function carregarDados() {
+            try{
+                const dados = await buscarProdutos();
+                setProdutos(dados);
+            } catch (e){
+                setErro(e.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        carregarDados();
+    }, []);
+    return {produtos, loading, erro};
+}
