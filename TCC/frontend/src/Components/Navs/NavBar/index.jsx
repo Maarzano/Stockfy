eb62b9aac496da6cccbcf2584b937412360e5850
Arthur Alves <alves2araujo@gmail.com>
2025-05-26 21:46:16 -0300

    Adicionando Animação de mudar a NavBar ao scrollar

--- TCC/frontend/src/Components/Navs/NavBar/index.jsx
+++ TCC/frontend/src/Components/Navs/NavBar/index.jsx
@@ -1,28 +1,42 @@
-import styled from "styled-components"
-import CardPerfil from "../../Cards/CardPerfil"
-import Cart from "../../../Assets/SVGs/Icons/icon-cart-black.svg"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import CardPerfil from "../../Cards/CardPerfil";
+import Cart from "../../../Assets/SVGs/Icons/icon-cart-black.svg";
+import { Link } from "react-router-dom";
 
-const NavBar = () =>{
-    
-    
+const NavBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <Wrapper>
+        <Wrapper className={scrolled ? "scrolled" : ""}>
             <WrapperCart>
                 <Link to="/Cart">
-                    <CartImg src={Cart}/>
+                    <CartImg src={Cart} />
                 </Link>
             </WrapperCart>
             <Link to="/Gallery">
-                <SystemName>
-                    Stockify
-                </SystemName>
+                <SystemName>Stockify</SystemName>
             </Link>
-            <CardPerfil/>
+            <CardPerfil />
         </Wrapper>
-    )
-}
+    );
+};
 
+// Estilo base
 const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
@@ -33,26 +47,35 @@ const Wrapper = styled.div`
     width: 100%;
     position: fixed;
     top: 0;
-`
+    transition: 0.5s all ease;
+    z-index: 1000;
+
+    &.scrolled {
+        top: 20px;
+        border-radius: 20px;
+        margin: 0px 50px 0px 50px;
+        width: 95%;
+    }
+`;
 
 const WrapperCart = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: left;
-`
+`;
 
 const CartImg = styled.img`
     width: 55px;
     margin-left: 85px;
     cursor: pointer;
-`
+`;
 
 const SystemName = styled.h1`
     color: #FFF;
     font-size: 60px;
     height: 100px;
     cursor: pointer;
-`
+`;
 
-export default NavBar
+export default NavBar;
