e470c06448a0e197dfb4b5649c9663bdacdb8f71
Arthur Alves <alves2araujo@gmail.com>
2025-06-08 12:15:01 -0300

    Tornado o sistema de pesquisa funcional

--- TCC/frontend/src/Components/Searchs/Search2/index.jsx
+++ TCC/frontend/src/Components/Searchs/Search2/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Search2 = () => {
+const Search2 = ({ value, onChange, placeholder = "Pesquisar..." }) => {
   return (
     <StyledWrapper>
       <div className="navbar-container">
@@ -9,24 +9,46 @@ const Search2 = () => {
           <div className="InputContainer">
             <svg className="searchIcon" width="20px" viewBox="0 0 24 24" height="20px" xmlns="http://www.w3.org/2000/svg">
               <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M15.5 14h-.79l-.28-.27A6.518 6.518 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+              <path d="M15.5 14h-.79l-.28-.27A6.518 6.518 0 0 0 16 9.5 
+                       6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 
+                       4.23-1.57l.27.28v.79l5 4.99L20.49 
+                       19l-4.99-5zm-6 0C7.01 14 5 11.99 
+                       5 9.5S7.01 5 9.5 5 14 7.01 14 
+                       9.5 11.99 14 9.5 14z" />
             </svg>
-            <input className="input" id="search-input" placeholder="Search..." type="text" />
+            <input
+              className="input"
+              id="search-input"
+              placeholder={placeholder}
+              type="text"
+              value={value}
+              onChange={onChange}
+            />
           </div>
           <div className="border" />
           <button aria-label="Voice search" className="micButton">
             <svg width="20px" viewBox="0 0 384 512" height="20px" className="micIcon" xmlns="http://www.w3.org/2000/svg">
-              <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z" />
+              <path d="M192 0C139 0 96 43 96 96V256c0 53 
+                       43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 
+                       216c0-13.3-10.7-24-24-24s-24 10.7-24 
+                       24v40c0 89.1 66.2 162.7 152 
+                       174.4V464H120c-13.3 0-24 10.7-24 
+                       24s10.7 24 24 24h72 72c13.3 0 
+                       24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 
+                       152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 
+                       10.7-24 24v40c0 70.7-57.3 128-128 
+                       128s-128-57.3-128-128V216z" />
             </svg>
           </button>
         </div>
       </div>
     </StyledWrapper>
   );
-}
+};
 
 const StyledWrapper = styled.div`
-    width: 90%;
+  width: 90%;
+
   .navbar-container {
     display: flex;
     justify-content: center;
@@ -129,6 +151,7 @@ const StyledWrapper = styled.div`
 
   .micButton:hover .micIcon {
     fill: #ff5100;
-  }`;
+  }
+`;
 
 export default Search2;
