488e97f50e7082895dcba3063bab31e51333724b
Marzano <arthuramarzano10@gmail.com>
2025-06-11 15:31:38 -0300

    envolvendo app com o cetexto do carrinho

--- TCC/frontend/src/App.jsx
+++ TCC/frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
+import { CartProvider } from "./Context/Cart";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes/>
+      <CartProvider>
+        <AppRoutes/>
+      </CartProvider>
     </BrowserRouter>
   );
 }
